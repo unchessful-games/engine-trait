1ccdab03fc211caa6d2e5468e92b1e10a8e1b76e
Danya Generalov <danya@danya02.ru>
2023-11-01 01:49:45 +0300

    Adding type-erased versions of API responses.

--- src/server_types.rs
+++ src/server_types.rs
@@ -1,6 +1,7 @@
 #[cfg(feature = "server")]
 use axum::{http::StatusCode, response::IntoResponse, Json};
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
 use shakmaty::{uci::Uci, Chess};
 
 use crate::Engine;
@@ -48,6 +49,19 @@ pub struct EngineInfo<E: Engine> {
     pub initial_state: E::State,
 }
 
+/// Type-erased [`EngineInfo`], where the engine-specific fields have been replaced with [`serde_json::Value`].
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct AnyEngineInfo {
+    /// The engine's algorithm ID.
+    pub id: String,
+
+    /// A human-readable description of what the engine does.
+    pub description: String,
+
+    /// Initial state value. Pass this when making a move.
+    pub initial_state: Value,
+}
+
 /// Errors relating to a submitted request, independent of the engine.
 #[derive(Serialize, Deserialize, Clone, Debug)]
 #[non_exhaustive]
@@ -92,11 +106,48 @@ pub struct EngineResponse<E: Engine> {
     pub engine_state: E::State,
 }
 
+/// Type-erased [`EngineResponse`], where the engine-specific fields have been replaced with [`serde_json::Value`].
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct AnyEngineResponse {
+    /// The move that the engine chose.
+    #[serde(with = "crate::chess_serde::uci_serde")]
+    pub r#move: Uci,
+
+    /// The game state after this move was played.
+    #[serde(with = "crate::chess_serde::position_serde")]
+    pub game_after: Chess,
+
+    /// The engine's status info about this move.
+    /// It is None if the request asked for no status info.
+    pub status_info: Option<Value>,
+
+    /// The random number we gave to the engine when it was observing the previous move.
+    /// None if it did not observe the previous move.
+    pub observe_other_rand_used: Option<u64>,
+
+    /// The random number we gave to the engine when it was producing this move.
+    pub produce_rand_used: u64,
+
+    /// The random number used to observe the move the engine had made.
+    pub observe_mine_rand_used: u64,
+
+    /// The engine's state. You need to pass this again if you want to continue this game.
+    pub engine_state: Value,
+}
+
 #[derive(Clone, Debug)]
 pub enum EngineResult<E: Engine> {
     RequestError(EngineRequestError),
     EngineError(E::Error),
     Ok(EngineResponse<E>),
+}
+
+/// Type-erased [`EngineResult`], where the engine-specific fields have been replaced with [`serde_json::Value`].
+#[derive(Clone, Debug)]
+pub enum AnyEngineResult {
+    RequestError(EngineRequestError),
+    EngineError(Value),
+    Ok(AnyEngineResponse),
 }
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
